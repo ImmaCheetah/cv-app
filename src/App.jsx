94ec4ca1547c81880254bf8e450b449eddefd07f
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-07-05 11:19:24 -0400

    feat: refactor to use object with single event handler

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,33 @@ import PersonalDetails from './components/PersonalDetails'
 
 
 function App() {
-  const [firstName, setFirstName] = useState('')
+
+  const [user, setUser] = useState({
+    firstName: 'Bob',
+    lastName: 'Job',
+    email: 'bob@email.com',
+    phone: '123456',
+    address: '123 Main St',
+  })
 
   function handleChange(e) {
-    setFirstName(e.target.value)
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    })
   }
+  
 
   return (
-    <>
-      <PersonalDetails onChange={handleChange}/>
-      <h2>{firstName}</h2>
-    </>
+    <div className='main-container'>
+      <div className="user-input-container">
+        <PersonalDetails {...user} onChange={handleChange}/>
+      </div>
+      <div className="user-output-container">
+        <h2>{user.firstName} </h2>
+        <h3>{user.lastName}</h3>
+      </div>
+    </div>
   )
 }
 
--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -1,11 +1,10 @@
 export default function PersonalDetails(props) {
 
-
     return (
-        <div className="personal-details-div" onChange={props.onChange}>
+        <div className="personal-details-div" >
             <h1>Personal Details</h1>
-            <input type="text" placeholder="First name"/>
-            <input type="text" placeholder="Last name"/>
+            <input name="firstName" type="text" placeholder="First name" value={props.firstName} onChange={props.onChange}/>
+            <input type="text" placeholder="Last name" value={props.lastName} onChange={props.onChange}/>
             <input type="email" placeholder="Email" />
             <input type="tel" placeholder="Phone Number"/>
             <input type="text" placeholder="Address"/>
