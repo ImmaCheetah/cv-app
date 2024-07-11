d888b39fff3829427f302b1a778371e5c473b1b5
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-07-11 13:51:25 -0400

    feat: add date formatting to show only month and year

--- src/components/EducationDisplay.jsx
+++ src/components/EducationDisplay.jsx
@@ -1,6 +1,16 @@
 import '../styles/section.css'
+import { format, parse } from "date-fns";
 
 export default function EducationDisplay(props) {
+
+    function formatDate(date) {
+        if (!date) {
+            return
+        } else {
+            return format(new Date(date), "MMM, yyyy")
+        }
+    }
+    
     const list = props.educationArray.map((education) => {
         return (
             <div className="display education-div">
@@ -9,7 +19,9 @@ export default function EducationDisplay(props) {
                     <p className='left-header-desc school'>{education.school}</p>
                 </div>
                 <div className="right-info">
-                    <p className='date'>{education.startDate} {education.endDate && '-'} {education.endDate}</p>
+                    <p className='date'>
+                        {formatDate(education.startDate)} {education.endDate && '-'} {formatDate(education.endDate)}
+                    </p>
                     <p className='location'>{education.location}</p>
                 </div>
             </div>
--- src/components/ExperienceDisplay.jsx
+++ src/components/ExperienceDisplay.jsx
@@ -1,6 +1,16 @@
 import '../styles/section.css'
+import { format, parse } from "date-fns";
 
 export default function ExperienceDisplay(props) {
+
+    function formatDate(date) {
+        if (!date) {
+            return
+        } else {
+            return format(new Date(date), "MMM, yyyy")
+        }
+    }
+
     const list = props.experienceArray.map((experience) => {
         return (
             <div className="display experience-div">
@@ -10,7 +20,9 @@ export default function ExperienceDisplay(props) {
                     <p className="description">{experience.description}</p>
                 </div>
                 <div className="right-info">
-                    <p className='date'>{experience.startDate} {experience.endDate && '-'} {experience.endDate}</p>
+                    <p className='date'>
+                        {formatDate(experience.startDate)} {experience.endDate && '-'} {formatDate(experience.endDate)}
+                    </p>
                     <p className="location">{experience.location}</p>
                 </div>
             </div>
